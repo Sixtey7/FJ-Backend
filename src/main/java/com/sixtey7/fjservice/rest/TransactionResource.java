98b2696bb45d04cab21f94f042c3fa2b89ce5e20
Patrick Shaw <sixtey7@gmail.com>
2019-02-15 21:45:39 -0700

    add some comments to the new method

--- src/main/java/com/sixtey7/fjservice/rest/TransactionResource.java
+++ src/main/java/com/sixtey7/fjservice/rest/TransactionResource.java
@@ -122,7 +122,12 @@ public class TransactionResource {
 
         return Response.status(200).entity(newId).build();
     }
-
+    /**
+     * REST Service used to import transactions for an account
+     * @param accountId The id of the account to assign the transactions to
+     * @param transactionData The data to import (comma seperated)
+     * @return {@link Response} containing the number of transactions imported
+     */
     @Path("/import/{accountId}")
     @PUT
     @Consumes(MediaType.TEXT_PLAIN)
