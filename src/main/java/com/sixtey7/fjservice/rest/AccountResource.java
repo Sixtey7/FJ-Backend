dc1838ce35211202d5466df20e141f42d5124bf3
Patrick Shaw <sixtey7@gmail.com>
2019-03-15 20:50:27 -0400

    add logging to account resource

--- src/main/java/com/sixtey7/fjservice/rest/AccountResource.java
+++ src/main/java/com/sixtey7/fjservice/rest/AccountResource.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sixtey7.fjservice.model.Account;
 import com.sixtey7.fjservice.model.db.AccountDAO;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -21,6 +23,9 @@ import java.util.List;
 @RequestScoped
 public class AccountResource {
 
+    //Create a logger for the class
+    private static final Logger LOGGER = LogManager.getLogger(AccountResource.class);
+
     /**
      * DAO Used to interact with the database
      */
@@ -35,6 +40,7 @@ public class AccountResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public JsonObject test() {
+        LOGGER.info("Returning Health Check");
         return Json.createObjectBuilder()
                 .add("message", "Account Service Up and Running!")
                 .build();
@@ -48,8 +54,10 @@ public class AccountResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAllAccounts() {
+        LOGGER.info("Returning all accounts");
         List<Account> allAccounts = dao.getAllAccounts();
 
+        LOGGER.debug("Returning {} accounts", allAccounts.size());
         try {
             ObjectMapper om = new ObjectMapper();
             String returnString = om.writeValueAsString(allAccounts);
@@ -71,6 +79,7 @@ public class AccountResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getOneAccount(@PathParam("accountId") final String accountId) {
+        LOGGER.info("Returning account details for id {}", accountId);
         try {
             Account account = dao.getAccount(accountId);
 
@@ -93,11 +102,15 @@ public class AccountResource {
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     public Response addAccount(Account account) {
+        LOGGER.info("Adding a new account");
         if (account.getId() != null) {
+            LOGGER.warn("PUT method was called to for existing id {}", account.getId());
             return Response.status(400).entity("Use POST method if updating").build();
         }
 
         String newId = dao.addAccount(account);
+
+        LOGGER.debug("Assigned ID {}", newId);
         
         return Response.status(200).entity(newId).build();
     }
@@ -112,7 +125,9 @@ public class AccountResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public Response updateAccount(@PathParam("accountId") final String accountId, final Account account) {
+        LOGGER.info("Updating account info for account id: {}", account);
         if (accountId == null) {
+            LOGGER.warn("Account id was not provided to POST method");
             return Response.status(400).entity("ID is required as part of the path!").build();
         }
 
@@ -122,6 +137,7 @@ public class AccountResource {
             return Response.status(200).build();
         }
         else {
+            LOGGER.error("Failed to save the account update for account id {}", accountId);
             return Response.status(500).entity("Failed to save account update!").build();
         }
 
@@ -136,8 +152,10 @@ public class AccountResource {
     @DELETE
     @Produces(MediaType.TEXT_PLAIN)
     public Response deleteAccount(@PathParam("accountId") final String accountId) {
+        LOGGER.info("Deleting account with id: {}", accountId);
         int response = dao.deleteAccount(accountId);
 
+        LOGGER.debug("Deleted {} accounts", response);
         return Response.status(200).entity(response).build();
     }
 
@@ -148,8 +166,10 @@ public class AccountResource {
     @Path("")
     @DELETE
     public Response deleteAll() {
+        LOGGER.info("Deleting all accounts!");
         int response = dao.deleteAllAccounts();
 
+        LOGGER.debug("Deleted {} accounts", response);
         return Response.status(200).entity(response).build();
     }
 }
