d0273fd198c34ae900aacc1a341e177bd103418b
Patrick Shaw <sixtey7@gmail.com>
2019-04-25 06:32:42 -0400

    implement an initial CORS Provider

---
+++ src/main/java/com/sixtey7/fjservice/rest/CORSProvider.java
@@ -0,0 +1,23 @@
+package com.sixtey7.fjservice.rest;
+
+
+import org.jboss.resteasy.plugins.interceptors.CorsFilter;
+
+import javax.ws.rs.core.Feature;
+import javax.ws.rs.core.FeatureContext;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class CORSProvider implements Feature {
+
+    @Override
+    public boolean configure(FeatureContext context) {
+        CorsFilter filter = new CorsFilter();
+        filter.getAllowedOrigins().add("*");
+        filter.setAllowedMethods("GET, POST, PUT, OPTIONS, HEAD");
+        filter.setAllowedHeaders("accept, content-type, origin");
+        context.register(filter);
+        System.out.println("Running!");
+        return true;
+    }
+}
