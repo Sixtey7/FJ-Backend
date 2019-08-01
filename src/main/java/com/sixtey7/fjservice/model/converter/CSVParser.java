a4f8648f989066cf09b345edf5300ed7050f8090
Patrick Shaw <sixtey7@gmail.com>
2019-07-31 22:39:47 -0400

    keep working on the csv parser

--- src/main/java/com/sixtey7/fjservice/model/converter/CSVParser.java
+++ src/main/java/com/sixtey7/fjservice/model/converter/CSVParser.java
@@ -1,5 +1,6 @@
 package com.sixtey7.fjservice.model.converter;
 
+import com.sixtey7.fjservice.model.Transaction;
 import com.sixtey7.fjservice.model.db.AccountDAO;
 import com.sixtey7.fjservice.model.db.TransactionDAO;
 import com.sixtey7.fjservice.utils.AccountHelper;
@@ -9,6 +10,7 @@ import org.apache.logging.log4j.Logger;
 
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
+import java.util.UUID;
 
 /**
  * Class used to parse a generated CSV File
@@ -44,6 +46,16 @@ public class CSVParser {
      */
     @Inject
     private TransHelper txHelper;
+
+    /**
+     * Generates a single transaction from a line from the CSV File
+     * @param csvLine the line from the file
+     * @param accountUUID {@link UUID} of the account
+     * @return {@link Transaction} created from the String
+     */
+    private Transaction generateTxFromString(String csvLine, UUID accountUUID) {
+        return new Transaction();
+    }
 
 }
 
