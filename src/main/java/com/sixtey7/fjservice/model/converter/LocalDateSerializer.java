9dd5acffd20ca00fa11ec2b0536357b5079ea007
Patrick Shaw <sixtey7@gmail.com>
2020-06-27 21:57:50 -0600

    add a comment

--- src/main/java/com/sixtey7/fjservice/model/converter/LocalDateSerializer.java
+++ src/main/java/com/sixtey7/fjservice/model/converter/LocalDateSerializer.java
@@ -8,8 +8,12 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Serializer class used to serialize a LocalDate to JSON String
+ */
 public class LocalDateSerializer extends StdSerializer<LocalDate> {
 
+    // default Serial Version UID
     private static final long serialVersionUID = 1L;
 
     public LocalDateSerializer(){
